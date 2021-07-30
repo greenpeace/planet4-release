87a116b17cbaead95b9f1381f5f8f95137416966
Nikos Roussos <nikos@roussos.cc>
2021-07-30 10:46:42 +0300

    Add deploy CI config and basic src

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import json
+import os
+import requests
+
+def main(request):
+    request_json = request.get_json()
+
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
